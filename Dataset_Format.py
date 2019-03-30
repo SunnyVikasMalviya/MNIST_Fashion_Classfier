5b51ad406472d782d2b8dd69cf4b22eb44773a0f
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-03-30 14:39:21 +0530

    Add files via upload

---
+++ Dataset_Format.py
@@ -0,0 +1,25 @@
+'''
+Format of the dataset
+'''
+
+#Display the dimensions of the training data
+print(train_images.shape)
+
+#Number of labels in the train_labels array
+print(len(train_labels))
+
+#Each label is an integer between 0 to 9
+print(train_labels)
+
+#Display the test_images dimensions i.e. the testing data
+print(test_images.shape)
+
+#Number of labels in the test_labels array
+print(test_labels)
+
+'''
+The whole MNIST Fashion dataset is divided into a test and train parts of
+60,000 and 10,000 images.
+The images are represented as 28*28 pixels.
+'''
+
