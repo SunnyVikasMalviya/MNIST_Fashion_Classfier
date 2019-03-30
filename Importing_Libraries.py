c36b7c9c2286663f89140545df725cde71a41f42
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2019-03-30 14:38:42 +0530

    Add files via upload

---
+++ Importing_Libraries.py
@@ -0,0 +1,11 @@
+#Tensorflow and keras libraries
+import tensorflow as tf
+from tensorflow import keras
+
+#Supporting libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Display version of tensorflow
+print(tf.__version__)
